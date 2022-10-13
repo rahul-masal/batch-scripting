421a2b35438d8890d2670d576a5605684ca448c5
Rahul Masal <61948407+rahul-masal@users.noreply.github.com>
2022-10-13 09:01:04 +0000

    Add files via upload

---
+++ multipleChromeTabs.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+start "Chrome" "C:\Program Files\Google\Chrome\Application\chrome.exe" --profile-directory="Profile 1"
+start "Chrome" "C:\Program Files\Google\Chrome\Application\chrome.exe" --profile-directory="Profile 2"
+Exit
+
+
